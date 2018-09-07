fc4593600f98988df4f3f7270363600fef18ed71
agentproxy <ericjoseph.flores1@gmail.com>
2018-09-07 08:57:45 +0800

    content partial, design started

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
-var projects = {
+export const projects = {
     grindingGary: {
         name: 'Grinding Gary',
-        dateCreated: '2017-1-2'
-    }  
+        dateCreated: '2017-1-2',
+        description: "It is a Javascript-based game that was created for a project in college"
+    },
+    pepsi: {
+        name: 'PCPPI Careers',
+        dateCreated: '2017-1-2',
+        description: "It is a website created during my OJT days in Pepsi."
+    }   
 }
