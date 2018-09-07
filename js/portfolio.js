667d30689980912c9c8039730ad4e344bdc2d644
agentproxy <ericjoseph.flores1@gmail.com>
2018-09-07 09:32:04 +0800

    added folders for better structuring

--- js/portfolio.js
+++ js/portfolio.js
@@ -17,10 +17,13 @@ var  projects = {
     kingsCup:{
         name: "King's Cup",
         dateCreated: "2015",
+        status: "Local",
         description: "A terminal-based game that was made to mimic the King's Cup drinking game without the cards."
     },
     grindingGary: {
         name: 'Grinding Gary',
+        status: "Deployed",
+        link: "www.grindinggary.000webhostapp.com",
         dateCreated: '2017-1-2',
         description: "It is a Javascript-based game that was created for a project in college. It was created together with Nuj Valaquio."
     },
@@ -29,6 +32,11 @@ var  projects = {
         dateCreated: '2017-1-2',
         description: "It is a website created during my OJT days in Pepsi."
     }  
+    //drunk dave
+    //tiyangge dash
+    //Ball Time
+    //Orgy
+    //
 }
 
 console.log(window.innerWidth,window.innerHeight)
