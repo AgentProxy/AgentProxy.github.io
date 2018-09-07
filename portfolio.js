c8c575cc3df416e5e356b82c29ee6086370c128d
agentproxy <ericjoseph.flores1@gmail.com>
2018-09-07 09:15:41 +0800

    index renamed

--- portfolio.js
+++ portfolio.js
@@ -11,42 +11,24 @@ var views = {
             block: 'start'
         });
     }
-    // }
-    // showHome: function(){
-        
-    //     document.getElementById('home').style.display = "block";
-    //     // document.getElementById(currentPage).style.display = "block";
-    //     // window.alert('whoa');
-    //     // document.getElementById('project-name').innerHTML = projects.grindingGary.name;
-    //     currentPage = 'home';
-    // },
-    // showProjects: function(){
-        
-    //     document.getElementById('projects').style.display = "block";
-    //     document.getElementById(currentPage).style.display = "none";
-    //     // window.alert('whoa');
-    //     document.getElementById('project-name').innerHTML = projects.pepsi.name;
-    //     currentPage = 'projects';
-    // },
-    // showAboutMe: function(){
-    //     document.getElementById('about-me').scrollIntoView({
-    //         behavior: 'smooth',
-    //         block: 'start' // scroll to top of target element
-    //     })
-    // }
 }
 
 var  projects = {
+    kingsCup:{
+        name: "King's Cup",
+        dateCreated: "2015",
+        description: "A terminal-based game that was made to mimic the King's Cup drinking game without the cards."
+    },
     grindingGary: {
         name: 'Grinding Gary',
         dateCreated: '2017-1-2',
-        description: "It is a Javascript-based game that was created for a project in college"
+        description: "It is a Javascript-based game that was created for a project in college. It was created together with Nuj Valaquio."
     },
     pepsi: {
         name: 'PCPPI Careers',
         dateCreated: '2017-1-2',
         description: "It is a website created during my OJT days in Pepsi."
-    }   
+    }  
 }
 
 console.log(window.innerWidth,window.innerHeight)
